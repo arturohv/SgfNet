a116a709d0268555c092d61e8669cc6846039d57
arturohv <arturohv@gmail.com>
2014-05-27 15:56:28 -0600

    Cambios en metadata

--- SGF.net/Models/Metadata/officesMetadata.cs
+++ SGF.net/Models/Metadata/officesMetadata.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using System.Web.Mvc;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SGF.net.Models
 {
