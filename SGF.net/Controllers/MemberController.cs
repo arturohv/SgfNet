5b989d3f45c75922e43850748b7ef4e11c58fe26
arturohv <arturohv@gmail.com>
2014-05-29 16:33:31 -0600

    Se agrega el controler Member

---
+++ SGF.net/Controllers/MemberController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SGF.net.Models;
+
+namespace SGF.net.Controllers
+{
+    public class MemberController : Controller
+    {
+        private XenosENEntities db = new XenosENEntities();
+
+        //
+        // GET: /Member/
+
+        public ActionResult Index()
+        {
+            var members = db.members.Include(m => m.memberMaritalStatus).Include(m => m.memberPaymentTypes).Include(m => m.offices);
+            return View(members.ToList());
+        }
+
+        //
+        // GET: /Member/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            members members = db.members.Find(id);
+            if (members == null)
+            {
+                return HttpNotFound();
+            }
+            return View(members);
+        }
+
+        //
+        // GET: /Member/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.maritalStatusId = new SelectList(db.memberMaritalStatus, "maritalStatusId", "name");
+            ViewBag.paymentTypeId = new SelectList(db.memberPaymentTypes, "paymentTypeId", "name");
+            ViewBag.officeId = new SelectList(db.offices, "officeId", "officeName");
+            return View();
+        }
+
+        //
+        // POST: /Member/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(members members)
+        {
+            if (ModelState.IsValid)
+            {
+                db.members.Add(members);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.maritalStatusId = new SelectList(db.memberMaritalStatus, "maritalStatusId", "name", members.maritalStatusId);
+            ViewBag.paymentTypeId = new SelectList(db.memberPaymentTypes, "paymentTypeId", "name", members.paymentTypeId);
+            ViewBag.officeId = new SelectList(db.offices, "officeId", "officeName", members.officeId);
+            return View(members);
+        }
+
+        //
+        // GET: /Member/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            members members = db.members.Find(id);
+            if (members == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.maritalStatusId = new SelectList(db.memberMaritalStatus, "maritalStatusId", "name", members.maritalStatusId);
+            ViewBag.paymentTypeId = new SelectList(db.memberPaymentTypes, "paymentTypeId", "name", members.paymentTypeId);
+            ViewBag.officeId = new SelectList(db.offices, "officeId", "officeName", members.officeId);
+            return View(members);
+        }
+
+        //
+        // POST: /Member/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(members members)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(members).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.maritalStatusId = new SelectList(db.memberMaritalStatus, "maritalStatusId", "name", members.maritalStatusId);
+            ViewBag.paymentTypeId = new SelectList(db.memberPaymentTypes, "paymentTypeId", "name", members.paymentTypeId);
+            ViewBag.officeId = new SelectList(db.offices, "officeId", "officeName", members.officeId);
+            return View(members);
+        }
+
+        //
+        // GET: /Member/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            members members = db.members.Find(id);
+            if (members == null)
+            {
+                return HttpNotFound();
+            }
+            return View(members);
+        }
+
+        //
+        // POST: /Member/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            members members = db.members.Find(id);
+            db.members.Remove(members);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SGF.net/Models/Metadata/memberMetadata.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace SGF.net.Models
+{
+    [MetadataTypeAttribute(typeof(members.MemberMetadata))]
+    public partial class members
+    {
+        internal sealed class MemberMetadata
+        {
+
+            private MemberMetadata()
+            {
+            }
+
+            [Key]
+            [DisplayName("Id:")]   
+            public int memberId { get; set; }
+
+            [DisplayName("Cédula:")]
+            [Required]
+            public string documentId { get; set; }
+
+
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public int maritalStatusId { get; set; }
+            public System.DateTime birthdate { get; set; }
+            public string phoneNumber { get; set; }
+            public string cellNumber { get; set; }
+            public string address { get; set; }
+            public int officeId { get; set; }
+            public System.DateTime dischargedDate { get; set; }
+            public int paymentTypeId { get; set; }
+            public bool active { get; set; }
+
+            
+            //public virtual memberMaritalStatus memberMaritalStatus { get; set; }
+            //public virtual memberPaymentTypes memberPaymentTypes { get; set; }
+            //public virtual offices offices { get; set; }
+        }
+    }
+}
